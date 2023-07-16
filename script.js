b87e0b89a0c5b5305d574c7a154c1a36a6135801
Karan Tulsani <117897987+karan151120@users.noreply.github.com>
2023-07-16 15:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const hasKey = sampleObject.hasOwnProperty(key);
+  
+  if(hasKey) console.log(true);
+  else console.log(false);
 }
 
 // Do not change the code below
